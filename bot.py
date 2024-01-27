522421ed69fb734cbb3ef6544fea9433ad124a95
tcnickless <tcnickless@gmail.com>
2024-01-27 23:41:16 +0000

    debugging client connection

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ import os
 import discord
 from dotenv import load_dotenv
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 load_dotenv()
 TOKEN  = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
