0c30233b920231c3c30278dfbf599c95529923f8
tcnickless <tcnickless@gmail.com>
2024-01-28 00:53:01 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_ready():
 
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{guild.name} (id: {guild.id})'
     )
 
 client.run(TOKEN)
