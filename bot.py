15dbdfe76d1a83b3edd1a4cf36f29eeee994a4ab
tcnickless <tcnickless@gmail.com>
2024-01-28 01:28:16 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,21 +3,20 @@ import os
 import discord
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
         f'{client.user} is connected to the following guild:\n'
