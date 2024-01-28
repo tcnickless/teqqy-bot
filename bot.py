a7358f233273849696433bb8a4b5d6c75c978fd9
tcnickless <tcnickless@gmail.com>
2024-01-28 00:30:30 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import os
 import discord
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-intents.message_content = True
+#intents = discord.Intents.default()
+#intents.message_content = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=intents)
+client = discord.Client()
 
 @client.event
 async def on_ready():
