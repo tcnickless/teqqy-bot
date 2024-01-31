4283cc0938908563b821b38a9fdfd6f16411f60c
tcnickless <tcnickless@gmail.com>
2024-01-31 13:25:57 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
