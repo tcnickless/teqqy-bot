04819f32a9bf7a1b10f66668c2c04701c6770a9c
tcnickless <tcnickless@gmail.com>
2024-02-01 02:17:32 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,6 +49,13 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
     
+@bot.command(name='roll_dice', help='Simulates a Dice Roll.')
+async def roll(ctx, number_of_dice, number_of_sides):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(','.join(dice))
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
